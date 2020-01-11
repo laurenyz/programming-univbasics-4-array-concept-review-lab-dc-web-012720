d7076c61d46a66361869f83db0ca9ab5456996f9
Lauren Yu <laurenkyu@gmail.com>
2020-01-11 14:31:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,19 +1,19 @@
-def find_element_index(array, value_to_find)
-  if array.include?(value_to_find)
-    array.index(value_to_find)
-  else 
-    return nil
-  end
-end
+# def find_element_index(array, value_to_find)
+#   if array.include?(value_to_find)
+#     array.index(value_to_find)
+#   else 
+#     return nil
+#   end
+# end
 
-def find_max_value(array)
-  new_array = array.sort 
-  new_array.last
-end
+# def find_max_value(array)
+#   new_array = array.sort 
+#   new_array.last
+# end
 
-def find_min_value(array)
-  new_array = array.sort
-  new_array.first
-end
+# def find_min_value(array)
+#   new_array = array.sort
+#   new_array.first
+# end
 
 
