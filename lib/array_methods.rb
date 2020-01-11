628dcb3951e638ade3e15088cb392bb9970825b3
Lauren Yu <laurenkyu@gmail.com>
2020-01-11 14:38:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -19,7 +19,7 @@
 
 def find_element_index(array, value_to_find)
   count = 0
-  array.length.times do
+  while count < array.length do 
     if array[count] == value_to_find
       return count
       count += 1 
